4ee410d0aa388298036f5b775370106827264bce
andresrdu <andresrdu@hotmail.com>
2019-10-24 19:00:31 -0500

    cambios2

--- lab1/devops/Jenkinsfile3Credentials.groovy
+++ lab1/devops/Jenkinsfile3Credentials.groovy
@@ -1,4 +1,23 @@
 
 node{
-    
+    stage("init"){
+        s
+    }
+    stage("checkout"){
+        withCredentials([string(credentialsId:"andresrdu-token",variable:'token1')]){
+            echo "${token1}"
+
+            tokentmp = token1.split('\\|')
+            tok1 = tokentmp[0]
+            tok2 = tokentmp[1]
+            tok3 = tokentmp[2]
+
+            echo "${tok1} ${tok2} ${tok3}"
+        }
+    }
 }
+
+/*
+password2019|andresrdu@hotmail.com|12345
+
+*/
